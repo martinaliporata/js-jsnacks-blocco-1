4b9f7ed8e457bcdad27e57a5fbb25a2ac4477264
martinaliporata <martinaliporata@gmail.com>
2024-05-13 11:57:09 +0200

    added ,10 for parseint

--- Snack 1/script.js
+++ Snack 1/script.js
@@ -1,13 +1,15 @@
 // L'utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore.
 
-const number1 = parseInt (prompt('Dimmi un numero'));
+const number1 = parseInt (prompt('Dimmi un numero'), 10);
 console.log(number1)
 
-const number2 = parseInt (prompt('Dimmi un altro numero'));
+const number2 = parseInt (prompt('Dimmi un altro numero'), 10);
 console.log(number2)
 
-if (number1 > number2) {
+if (number1 === number2) {
+    console.log (number1, number2)
+} else if (number1 > number2) {
     console.log(number1)
 } else {
     console.log(number2)

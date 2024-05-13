a9979edbc0a5ff02f36a95a8be491969c02bda1b
martinaliporata <martinaliporata@gmail.com>
2024-05-13 11:46:07 +0200

    added two const

---
+++ Snack 1/script.js
@@ -0,0 +1,9 @@
+// L'utente inserisce due numeri in successione, con due prompt.
+// Il software stampa il maggiore.
+
+const number1 = prompt('Dimmi un numero')
+console.log(number1)
+
+const number2 = prompt('Dimmi un altro numero')
+console.log(number2)
+

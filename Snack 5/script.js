f175073642043a58b47766184c30cb9655f49873
martinaliporata <martinaliporata@gmail.com>
2024-05-13 12:29:27 +0200

    added array and prompts

---
+++ Snack 5/script.js
@@ -0,0 +1,14 @@
+// Crea un array vuoto.
+//  Chiedi per 6 volte all'utente di inserire un numero,
+//  se è dispari inseriscilo nell'array.
+
+const array = []
+
+let number1 = parseInt (prompt('Dimmi un numero'))
+let number2 = parseInt (prompt('Dimmi un numero'))
+let number3 = parseInt (prompt('Dimmi un numero'))
+let number4 = parseInt (prompt('Dimmi un numero'))
+let number5 = parseInt (prompt('Dimmi un numero'))
+let number6 = parseInt (prompt('Dimmi un numero'))
+
+if (nu)

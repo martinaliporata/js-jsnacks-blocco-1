6dac203c53ac393c18bbc452f342397b76a51ec1
martinaliporata <martinaliporata@gmail.com>
2024-05-13 12:04:54 +0200

    added array for people invited and prompt for the user

---
+++ Snack 4/script.js
@@ -0,0 +1,16 @@
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
+// chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+const invitati = ["martina", "tiziano", "marco", "luca", "diego"]
+
+invitati[0]
+invitati[1]
+invitati[2]
+invitati[3]
+invitati[4]
+
+
+
+const nomeUtente = prompt('qual è il tuo nome')
+
+if nomeUtente

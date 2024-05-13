9a49cd692f6dca974af263ce559b9032487bdb70
martinaliporata <martinaliporata@gmail.com>
2024-05-13 12:25:05 +0200

    added includes solution

--- Snack 4/script.js
+++ Snack 4/script.js
@@ -1,17 +1,15 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
 // chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
 
-const array = ["martina", "tiziano", "marco", "luca", "diego"]
-
-array[0]
-array[1]
-array[2]
-array[3]
+const array = ["martina", "tiziano", "marco", "luca", "diego"];
+array[0];
+array[1];
+array[2];
+array[3];
 array[4]
 
-
-
 const nomeUtente = prompt('qual è il tuo nome')
+nomeUtente = nomeUtente.charAt(0).toLocaleUpperCase() + 
 
 if (nomeUtente === array[0]) {
     console.log('Entra')
@@ -26,3 +24,10 @@ if (nomeUtente === array[0]) {
 } else {
     console.log ('Non entrare')
 }
+
+// meglio con includes
+// if (array.includes(nomeUtente)) {
+//     console.log('Join')
+// } else {
+//     console.log('No')
+// }

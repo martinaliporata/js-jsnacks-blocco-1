9906ea9e43ea7eb6b9b736b2cb298e3a500a4e26
martinaliporata <martinaliporata@gmail.com>
2024-05-13 11:47:14 +0200

    added parseint

--- Snack 1/script.js
+++ Snack 1/script.js
@@ -1,9 +1,9 @@
 // L'utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore.
 
-const number1 = prompt('Dimmi un numero')
+const number1 = parseInt (prompt('Dimmi un numero'));
 console.log(number1)
 
-const number2 = prompt('Dimmi un altro numero')
+const number2 = parseInt (prompt('Dimmi un altro numero'));
 console.log(number2)
 

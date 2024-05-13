5bce0c7186cfa5abea5e455f7b31e83de0ae0047
martinaliporata <martinaliporata@gmail.com>
2024-05-13 12:08:03 +0200

    added if

--- Snack 4/script.js
+++ Snack 4/script.js
@@ -1,16 +1,28 @@
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, 
 // chiedi all'utente il suo nome e comunicagli se può partecipare o no alla festa.
 
-const invitati = ["martina", "tiziano", "marco", "luca", "diego"]
+const array = ["martina", "tiziano", "marco", "luca", "diego"]
 
-invitati[0]
-invitati[1]
-invitati[2]
-invitati[3]
-invitati[4]
+array[0]
+array[1]
+array[2]
+array[3]
+array[4]
 
 
 
 const nomeUtente = prompt('qual è il tuo nome')
 
-if nomeUtente
+if (nomeUtente === array[0]) {
+    console.log('Entra')
+} else if (nomeUtente === array[1]) {
+    console.log('Entra')
+} else if (nomeUtente === array[2]) {
+    console.log('Entra')
+} else if (nomeUtente === array[3]) {
+    console.log('Entra')
+} else if (nomeUtente === array[4]) {
+    console.log('Entra')
+} else {
+    console.log ('Non entrare')
+}

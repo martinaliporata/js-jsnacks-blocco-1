079129a11b47b8ad004e526b3257395a28a7040e
martinaliporata <martinaliporata@gmail.com>
2024-05-14 11:35:14 +0200

    created DOM element

---
+++ Snack 8/script.js
@@ -0,0 +1,11 @@
+// Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l'altro di verde. 
+// Partendo da un array di numeri, stampiamo nell'id rosso i numeri dispari e in verde i numeri pari.
+
+// crea elemento nel DOM
+const main = document.querySelector(main.prova)
+const divRosso = document.createElement('div')
+divRosso.append
+
+
+
+const array = [1,2,3,4,5,6,7,8,9,10]

ca6b8bf58f73f8968c15806428034a46a54af589
martinaliporata <martinaliporata@gmail.com>
2024-05-14 10:59:22 +0200

    corrected

--- Snack 6/script.js
+++ Snack 6/script.js
@@ -1,5 +1,14 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
-const array = [6, 8, 9, 13, 18, 23, 30]
+const numbers= [6, 8, 9, 13, 18, 23, 30];
+let somma = 0; 
 
-const dispari = array.at(1) + array.at(3) + array.at(4)
-console.log(dispari)
+// ciclo per ogni numero present nell'array
+for(let index = 0; index < numbers.length ; index++) {
+// verifico per ogni iterazione se l'indice su cui sto iterando è dispari
+    if (index % 2 === 1) {
+// se è dispari aggiungo elemento alla somma
+        somma += numbers[index];    
+    }
+}
+
+console.log(numbers, somma);

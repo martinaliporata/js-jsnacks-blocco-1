97062f4ef51cab76c8f7726cb585a9cb8c2a87e9
martinaliporata <martinaliporata@gmail.com>
2024-05-14 10:44:26 +0200

    added array.at and addidition

--- Snack 6/script.js
+++ Snack 6/script.js
@@ -1,3 +1,5 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 const array = [6, 8, 9, 13, 18, 23, 30]
 
+const dispari = array.at(1) + array.at(3) + array.at(4)
+console.log(dispari)

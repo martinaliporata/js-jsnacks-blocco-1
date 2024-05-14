012b99550f7d893fa4a7773e2c431fa207847301
martinaliporata <martinaliporata@gmail.com>
2024-05-14 17:35:30 +0200

    correct 2

--- Snack 8/script.js
+++ Snack 8/script.js
@@ -17,7 +17,7 @@
 // }
 
 
-// correzione (manca html. guarda registrazione)
+// correzione
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 const containerEl = document.querySelector('section.output')
